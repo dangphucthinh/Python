2473b0139185444eec1afdf024ddc82009e12ce3
Thinh(Oscar) P. DANG <tpdang@gosecure.net>
2021-12-14 15:38:36 +0700

    creating a new admin user

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    def __string__(self):
+    def __str__(self):
         return self.question_text
 
     #custom method model
@@ -17,5 +17,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __string__(self):
+    def __str__(self):
         return self.choice_text
